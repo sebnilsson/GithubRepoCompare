5fa7576917b1d9c800a61c0f3edefc955303380e
Sebastian Nilsson <sebastian.nilsson@offerta.se>
2016-08-03 16:39:54 +0200

    Removed SDK from global.json

--- src/GitHubRepoCompare.Website/Controllers/GitHubController.cs
+++ src/GitHubRepoCompare.Website/Controllers/GitHubController.cs
@@ -5,21 +5,21 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace GitHubRepoCompare.Website.Controllers
 {
-    [Route("github/[action]/{owner}/{repo}")]
-    public class GitHubController : Controller
-    {
-        [HttpGet]
-        public async Task<GitHubRepo> Repo(string owner, string repo)
-        {
-            var result = await GitHubApiService.GetRepo(owner, repo);
-            return result;
-        }
+    //[Route("github/[action]/{owner}/{repo}")]
+    //public class GitHubController : Controller
+    //{
+    //    [HttpGet]
+    //    public async Task<GitHubRepo> Repo(string owner, string repo)
+    //    {
+    //        var result = await GitHubApiService.GetRepo(owner, repo);
+    //        return result;
+    //    }
 
-        [HttpGet]
-        public async Task<IEnumerable<GitHubRepoContributors>> RepoContributors(string owner, string repo)
-        {
-            var result = await GitHubApiService.GetRepoContributors(owner, repo);
-            return result;
-        }
-    }
+    //    [HttpGet]
+    //    public async Task<IEnumerable<GitHubRepoContributors>> RepoContributors(string owner, string repo)
+    //    {
+    //        var result = await GitHubApiService.GetRepoContributors(owner, repo);
+    //        return result;
+    //    }
+    //}
 }
